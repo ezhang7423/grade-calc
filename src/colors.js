910cdadb3b73c552af594f82a251f870ef8851a6
ezhang7423 <ezhang7423@student.palomar.edu>
2020-03-24 11:47:58 -0700

    and the colors work

---
+++ src/colors.js
@@ -0,0 +1,79 @@
+function lighten(dat) {
+  let r, g, b, a;
+  [r, g, b, a] = dat;
+
+  if (r.length == 1) r = "0" + r;
+  if (g.length == 1) g = "0" + g;
+  if (b.length == 1) b = "0" + b;
+
+  // Make r, g, and b fractions of 1
+  r /= 255;
+  g /= 255;
+  b /= 255;
+
+  // Find greatest and smallest channel values
+  let cmin = Math.min(r, g, b),
+    cmax = Math.max(r, g, b),
+    delta = cmax - cmin,
+    h = 0,
+    s = 0,
+    l = 0;
+  // Calculate hue
+  // No difference
+  if (delta == 0) h = 0;
+  // Red is max
+  else if (cmax == r) h = ((g - b) / delta) % 6;
+  // Green is max
+  else if (cmax == g) h = (b - r) / delta + 2;
+  // Blue is max
+  else h = (r - g) / delta + 4;
+
+  h = Math.round(h * 60);
+
+  // Make negative hues positive behind 360°
+  if (h < 0) h += 360;
+  // Calculate lightness
+  l = (cmax + cmin) / 2;
+
+  // Calculate saturation
+  s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+
+  l += 0.3; // the key line
+
+  let c = (1 - Math.abs(2 * l - 1)) * s,
+    x = c * (1 - Math.abs(((h / 60) % 2) - 1)),
+    m = l - c / 2;
+  (r = 0), (g = 0), (b = 0);
+
+  if (0 <= h && h < 60) {
+    r = c;
+    g = x;
+    b = 0;
+  } else if (60 <= h && h < 120) {
+    r = x;
+    g = c;
+    b = 0;
+  } else if (120 <= h && h < 180) {
+    r = 0;
+    g = c;
+    b = x;
+  } else if (180 <= h && h < 240) {
+    r = 0;
+    g = x;
+    b = c;
+  } else if (240 <= h && h < 300) {
+    r = x;
+    g = 0;
+    b = c;
+  } else if (300 <= h && h < 360) {
+    r = c;
+    g = 0;
+    b = x;
+  }
+  r = Math.round((r + m) * 255);
+  g = Math.round((g + m) * 255);
+  b = Math.round((b + m) * 255);
+
+  return [r, g, b];
+  //return "hsl(" + h + "," + s + "%," + l + "%)"
+}
--- src/functions.js
+++ src/functions.js
@@ -1,52 +1,11 @@
 function print(thing) {
   console.log(thing);
 }
-function RGBToHSL(r, g, b, a) {
-  if (r.length == 1) r = "0" + r;
-  if (g.length == 1) g = "0" + g;
-  if (b.length == 1) b = "0" + b;
 
-  // Make r, g, and b fractions of 1
-  r /= 255;
-  g /= 255;
-  b /= 255;
-
-  // Find greatest and smallest channel values
-  let cmin = Math.min(r, g, b),
-    cmax = Math.max(r, g, b),
-    delta = cmax - cmin,
-    h = 0,
-    s = 0,
-    l = 0;
-  // Calculate hue
-  // No difference
-  if (delta == 0) h = 0;
-  // Red is max
-  else if (cmax == r) h = ((g - b) / delta) % 6;
-  // Green is max
-  else if (cmax == g) h = (b - r) / delta + 2;
-  // Blue is max
-  else h = (r - g) / delta + 4;
-
-  h = Math.round(h * 60);
-
-  // Make negative hues positive behind 360°
-  if (h < 0) h += 360;
-  // Calculate lightness
-  l = (cmax + cmin) / 2;
-
-  // Calculate saturation
-  s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
-
-  // Multiply l and s by 100
-  s = +(s * 100).toFixed(1);
-  l = +(l * 100).toFixed(1);
-  return [h, s, l];
-  //return "hsl(" + h + "," + s + "%," + l + "%)";
-}
 let alph = num => {
   return String.fromCharCode(97 + num);
 };
+
 function getColors(num) {
   let act = [];
   let colors = colormap({
@@ -56,19 +15,8 @@ function getColors(num) {
     alpha: 1
   });
   for (let x of colors) {
-    [h, s, l] = RGBToHSL(x);
-    print(h);
-    act.push(
-      "hsla(" +
-        h +
-        "," +
-        s +
-        "%," +
-        String(parseInt(l) + 30) +
-        "%," +
-        x[3] +
-        "%)"
-    );
+    [r, g, b] = lighten(x);
+    act.push("rgba(" + r + "," + g + "," + b + "," + x[3] + ")");
   }
   return act;
 }
